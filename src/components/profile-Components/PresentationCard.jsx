66de2177b60cadb32cf2d682251a0b09fa5812db
Tommaso Bricchetti <106075600+TommasoBrichetti@users.noreply.github.com>
2022-09-19 15:34:32 +0200

    profile bozza

---
+++ src/components/profile-Components/PresentationCard.jsx
@@ -0,0 +1,9 @@
+
+
+const PresentationCard = (props) => {
+    return(
+        <div></div>
+    )
+}
+
+export default PresentationCard
---
+++ src/components/profile-Components/Profile.jsx
@@ -0,0 +1,8 @@
+
+const Profile = (props) => {
+    return(
+        <div></div>
+    )
+}
+
+export default Profile
