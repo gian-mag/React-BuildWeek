9ac0afe16546ce3cec0c018c3fa241ea0dfa2535
Tommaso Bricchetti <106075600+TommasoBrichetti@users.noreply.github.com>
2022-09-20 16:59:22 +0200

    fix fetch bug

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -91,7 +91,8 @@ export const getExperiencesAction = () => {
 
 export const postExperiencesAction = (data) => {
     let headers = {
-        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzI4MjFhYjZkNzlhNTAwMTUwOTAyZjAiLCJpYXQiOjE2NjM1NzQ0NDQsImV4cCI6MTY2NDc4NDA0NH0.19jtd3dROekp-6-Hfjt13YTBBrxtLhgeu4ZcI07tRt4'
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzI4MjFhYjZkNzlhNTAwMTUwOTAyZjAiLCJpYXQiOjE2NjM1NzQ0NDQsImV4cCI6MTY2NDc4NDA0NH0.19jtd3dROekp-6-Hfjt13YTBBrxtLhgeu4ZcI07tRt4',
+        "Content-Type": "application/json"
     }
 
     return async (dispatch, getState) => {
