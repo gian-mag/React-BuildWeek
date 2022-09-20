acb4131555e5e2ef7fc564c56afe22c32d141452
gian-mag <106075481+gian-mag@users.noreply.github.com>
2022-09-20 11:58:01 +0200

    css fix

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import Profile from './components/profile-Components/Profile'
 import Footer from './components/Footer.jsx'
 import './App.css';
 import NavBar from './components/NavBar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">      
-        <NavBar />
-        <Profile />
-        <Footer />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div><NavBar /> <Profile /> <Footer /></div>}/>
+        {/* <Route path="/" element={}/>
+        <Route path="/" element={}/> */}
+      </Routes>
+    </BrowserRouter>
   )
-  }
+}
 
 
 export default App;
--- src/components/expComponent/ExpCard.jsx
+++ src/components/expComponent/ExpCard.jsx
@@ -1,7 +1,6 @@
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
 import { useSelector } from 'react-redux'
 import { BiUserCircle } from 'react-icons/bi'
 import '../../style/ExpCard.css'
@@ -11,7 +10,7 @@ const ExpCard = (props) => {
   const experiences = useSelector((state) => state.experiences.experiences)
 
   return (
-    <Card className="cardHover">
+    <Card className="myCard">
 
 
       <CardContent>
@@ -23,14 +22,14 @@ const ExpCard = (props) => {
           </Typography>
           <div className="myButtonsSect">
             <div className="myBtnWrap">
-              <a href="#">
+              <a className="myLink" href="#">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" className="mercado-match iconExp" width="24" height="24" focusable="false">
                   <path d="M21 13h-8v8h-2v-8H3v-2h8V3h2v8h8z"></path>
                 </svg>
               </a>
             </div>
             <div className="myBtnWrap">
-              <a href="#">
+              <a className="myLink" href="#">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" className="mercado-match iconExp" width="24" height="24" focusable="false">
                   <path d="M21.13 2.86a3 3 0 00-4.17 0l-13 13L2 22l6.19-2L21.13 7a3 3 0 000-4.16zM6.77 18.57l-1.35-1.34L16.64 6 18 7.35z"></path>
                 </svg>
