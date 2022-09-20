5dcae244364c1b2061f741640e841b4a66ec7dd2
gian-mag <106075481+gian-mag@users.noreply.github.com>
2022-09-20 11:48:32 +0200

    Exp Card Button

--- src/components/expComponent/ExpCard.jsx
+++ src/components/expComponent/ExpCard.jsx
@@ -11,36 +11,59 @@ const ExpCard = (props) => {
   const experiences = useSelector((state) => state.experiences.experiences)
 
   return (
-    <Card>
-      <CardActionArea>
-        <CardContent>
+    <Card className="cardHover">
+
+
+      <CardContent>
+
+        <div className="myIconsWrapper">
           <Typography gutterBottom variant="h5" component="div">
             Esperienza
+
           </Typography>
-          {
-            experiences.map((experience, i) =>
-            (
-              <div key={i} className="expWrap">
+          <div className="myButtonsSect">
+            <div className="myBtnWrap">
+              <a href="#">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" className="mercado-match iconExp" width="24" height="24" focusable="false">
+                  <path d="M21 13h-8v8h-2v-8H3v-2h8V3h2v8h8z"></path>
+                </svg>
+              </a>
+            </div>
+            <div className="myBtnWrap">
+              <a href="#">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" className="mercado-match iconExp" width="24" height="24" focusable="false">
+                  <path d="M21.13 2.86a3 3 0 00-4.17 0l-13 13L2 22l6.19-2L21.13 7a3 3 0 000-4.16zM6.77 18.57l-1.35-1.34L16.64 6 18 7.35z"></path>
+                </svg>
+              </a>
+            </div>
 
-                <BiUserCircle className="userExp" />
-                <div>
-                  <Typography variant="body2" color="text.secondary">
-                    {experience.role}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {experience.company} - {experience.area}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {experience.description}
-                  </Typography>
-                </div>
+          </div>
+        </div>
+
+        {
+          experiences.map((experience, i) =>
+          (
+            <div key={i} className="expWrap">
+
+              <BiUserCircle className="userExp" />
+              <div>
+                <Typography variant="body2" color="text.secondary">
+                  {experience.role}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {experience.company} - {experience.area}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {experience.description}
+                </Typography>
               </div>
-            )
-            )
-          }
+            </div>
+          )
+          )
+        }
+      </CardContent>
 
-        </CardContent>
-      </CardActionArea>
+
     </Card>
   );
 }
