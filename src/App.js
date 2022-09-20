eaa78031948093b98916d63ceabf1b51af5618ed
gian-mag <106075481+gian-mag@users.noreply.github.com>
2022-09-20 09:42:47 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-<<<<<<< HEAD
 import Footer from './components/Footer.jsx'
-=======
-
->>>>>>> navbar
 import './App.css';
 import NavBar from './components/NavBar'
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <Footer />
-=======
     <div className="App">      
         <NavBar />
->>>>>>> navbar
+        
+        <Footer />
     </div>
   );
 }
