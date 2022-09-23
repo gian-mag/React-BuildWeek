69bce9127b4535495b72c45a5be18d181afa1028
gian-mag <106075481+gian-mag@users.noreply.github.com>
2022-09-23 16:53:19 +0200

    fix

--- src/components/fuffa/LookingforWork.jsx
+++ src/components/fuffa/LookingforWork.jsx
@@ -15,10 +15,10 @@ const LookingForWork = () => {
                 <h2>Ciao, stai cercando lavoro?</h2>
                 <p>Possiamo aiutarti a prepararti per la tua ricerca. Solo tu potrai vedere la tua risposta.</p>
                 <div className="buttonAreaLookingForWork">
-                    <a href="https://youtu.be/dQw4w9WgXcQ" target='_blank' rel="noreferrer" className="noUnderlinepls"><div className='button404'>
+                    <a href="https://youtu.be/dQw4w9WgXcQ" target='_blank' rel="noreferrer" className="noUnderlinepls"><div className='button404 buttonForWork'>
                         <p> si </p>
                     </div></a>
-                    <div className='button404' onClick={idontwanttowork}>
+                    <div className='button404 buttonForWork' onClick={idontwanttowork}>
                         <p>no</p>
                     </div>
                 </div>
--- src/components/profile-Components/AllUserFetchCard.jsx
+++ src/components/profile-Components/AllUserFetchCard.jsx
@@ -14,10 +14,12 @@ const AllUserFetchCard = (props) => {
                         <span>{props.account.title}</span>
                         <div className="buttonArea">
                             
-                            <Button variant="outlined">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" data-supported-dps="16x16" fill="currentColor" className="mercado-match" width="16" height="16" focusable="false">
+                            <Button variant="outlined" className="userLinkati userIconati">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" data-supported-dps="16x16" fill="currentColor" className="mercado-match " width="16" height="16" focusable="false">
                                     <path d="M9 4a3 3 0 11-3-3 3 3 0 013 3zM6.75 8h-1.5A2.25 2.25 0 003 10.25V15h6v-4.75A2.25 2.25 0 006.75 8zM13 8V6h-1v2h-2v1h2v2h1V9h2V8z"></path>
-                                </svg>Collegati</Button>
+                                </svg>
+                                <span>Collegati</span> 
+                                </Button>
                         </div>
 
                     </div>
